ccbe6aedd5f8bc249dd794f1cd60a87ee8ec32e5
Alvin-Buana <alvinbuanaa@gmail.com>
2021-12-23 12:33:06 +0700

    Fixed Responsive Issue

--- hunting/src/script/Comp/Content.js
+++ hunting/src/script/Comp/Content.js
@@ -8,8 +8,8 @@ class Content extends HTMLElement {
   render() {
     this.setAttribute('class', 'col-12 col-sm-6 col-md-6 col-lg-4');
     this.innerHTML = `
-        <div class="card" style="width: 23rem; margin-bottom :10px;">
-          <img src="${this._place.Link}" class="card-img-top img-fluid" alt="${this._place.Place_Name}" style = "width: 22.9rem; height : 18rem;object-fit: cover;">
+        <div class="card" style="width: 100%; margin-bottom :10px;">
+          <img src="${this._place.Link}" class="card-img-top img-fluid" alt="${this._place.Place_Name}" style = "width: 100%; height : 18rem;object-fit: cover;">
             <div class="card-body">
               <h5 class="card-title" >${this._place.Place_Name}</h5>
                 <p class="card-text">${this._place.Category ? this._place.Category : '-'}</p>
